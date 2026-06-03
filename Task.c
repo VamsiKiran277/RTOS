5a4a0ae074cda9945864ccc41a240ddda19ac6dd
Vamsi Kiran <vamsikiran167@gmail.com>
2026-06-03 14:53:39 +0100

    Added fake address for the registers in the stack

--- Task.c
+++ Task.c
@@ -6,6 +6,17 @@ uint32_t TASKB_STACK[STACK_SIZE];
 
 uint32_t* TCB_Initialization(void(*task)(void),uint32_t*stack_arr) {
     uint32_t* topOfStack;
-    topOfStack = (stack_arr + (STACK_SIZE-1)); //to get the top address of the stack
-    return topOfStack;
+    topOfStack = (stack_arr + (STACK_SIZE)); //to get the top address of the stack
+    //fake addresses
+    topOfStack[-1] = 0x01000000; //standard as per ARM rules
+    topOfStack[-2] = (uint32_t)task; //address of next task function
+    topOfStack[-3] = 0x11111111; // Link register
+    topOfStack[-4] = 0x22222222; //R12
+    topOfStack[-5] = 0x33333333; //R3
+    topOfStack[-6] = 0x44444444; //R2
+    topOfStack[-7] = 0x55555555; //R1
+    topOfStack[-8] = 0xABCDEF01; //R0
+
+
+    return &topOfStack[-8];
 }  
